9500bed49f3903730ceb2dbfa62921a132cb0112
August <augustraro112@hotmail.com>
2021-02-18 13:43:17 +0100

    Changed some css and text inside alphabet-wrapper

--- src/components/AlphabetWrapper.js
+++ src/components/AlphabetWrapper.js
@@ -6,7 +6,7 @@ const AlphabetWrapper = () => {
   const [ letters, setLetters] = useState(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']);
   return (
     <div className={AlphaStyles.main_wrapper}>
-      <h3 className={AlphaStyles.h3}>Pick a letter</h3>
+      <h3 className={AlphaStyles.h3}>Guess a letter</h3>
       <div>
         {
           letters.map(letter => {
