a837d475176f3c8bb05cafc2c805b729576c2179
Johan Markström <johan.markstrom@gmail.com>
2021-02-18 14:38:09 +0100

    created word display component

---
+++ src/components/WordOutput.js
@@ -0,0 +1,16 @@
+import react from "react";
+import {style} from "../styles/WordOutput.module.css"
+
+const WordOutput = () => {
+    return (
+        <div className="WordOutput">
+            <div className="wordWrapper">
+                <div className="letterBox">
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default WordOutput;
