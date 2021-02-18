73cf8c7aca70a5e54836b88e3acf8241b4140163
Cedrik von Heiroth <cedrikvonheiroth@gmail.com>
2021-02-18 14:45:03 +0100

    Created StartGame component

---
+++ src/components/StartGame.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const StartGame = () => {
+    return ( 
+        <div className="startGame">
+            <button>Start game</button>
+        </div>
+    );
+}
+ 
+export default StartGame;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import HangmanDisplay from '../components/HangmanDisplay';
 import AlphabetWrapper from '../components/AlphabetWrapper';
 import LivesWrapper from '../components/LivesWrapper';
 import styles from '../styles/home.module.css';
+import StartGame from '../components/StartGame';
 
 const Home = () => {
   return (
@@ -12,11 +13,12 @@ const Home = () => {
           <h1>Welcome to Hangman</h1>
           <h3>The worlds most famous word game!</h3>
         </section>
-        <div className={styles.logo}></div>
+      <div className={styles.logo}></div>
       </div>
+        <StartGame />
         <AlphabetWrapper />
         <HangmanDisplay />
-      <LivesWrapper />
+        <LivesWrapper />
     </div>
   );
 }
