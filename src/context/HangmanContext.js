2da5d6ba9ed9c42942be35efe2125de14eaeff28
frida stenberg <fridastenberg97@hotmail.com>
2021-02-18 11:30:29 +0100

    Added a context, with array and smal random word function

---
+++ src/context/HangmanContext.js
@@ -0,0 +1,53 @@
+import { createContext, useState } from "react";
+export const HangmanContext = createContext;
+
+export function HangmanProvider (props) {
+    const [hangmans, setHangmans] = useState([
+        {
+            // adding boolean for possible logic use in game
+            hangman: "RandomWord1",
+            boolean: false
+        },
+        {
+            hangman: "RandomWord2",
+            boolean: false
+        },
+        {
+            hangman: "RandomWord3",
+            boolean: false
+        },
+        {
+            hangman: "RandomWord4",
+            boolean: false
+        },
+        {
+            hangman: "RandomWord5",
+            boolean: false
+        },
+        {
+            hangman: "RandomWord6",
+            boolean: false
+        },
+    ])
+
+    //Remeber to add hangmans as argumnet when calling on function
+    const randomWordFunc = (hangmans) => {
+        // Saving random word from array in variable for possible use
+        let hangmanWord = hangmans[Math.floor(Math.random() * hangmans.length)];
+    }
+
+const values = {
+    hangmans,
+    hangmanWord,
+    randomWordFunc
+}
+
+return (
+    <HangmanContext.Provider value={values}>
+        {/*Rember adding to app */}
+        {props.children}
+    </HangmanContext.Provider>
+)
+}
+
+export default HangmanProvider;
