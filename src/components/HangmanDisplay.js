68264055cf4bc58aec600ddd79c14d86b8ae4cbe
Chris <chrasse@hotmail.com>
2021-02-18 11:11:42 +0100

    Added some more parts to the Hangman

--- src/components/HangmanDisplay.js
+++ src/components/HangmanDisplay.js
@@ -6,6 +6,8 @@ const HangmanDisplay = () => {
       <p>Hangman</p>
       <div className={`${styles.hangmanContainer}`}>
         <div className={styles.foundation}></div>
+        <div className={styles.verticalPole}></div>
+        <div className={styles.horizontalPole}></div>
 
       </div>
     </div>
