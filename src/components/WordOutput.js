f413d5516435fc68258fc199465df6658ca5085a
Cedrik von Heiroth <cedrikvonheiroth@gmail.com>
2021-02-19 14:34:59 +0100

    small bug fix

--- src/components/WordOutput.js
+++ src/components/WordOutput.js
@@ -1,5 +1,5 @@
 import react from "react";
-import { style } from "../styles/WordOutput.module.css"
+import style from "../styles/WordOutput.module.css"
 import  { useContext } from "react";
 import { HangmanContext } from "../context/HangmanContext";
 
