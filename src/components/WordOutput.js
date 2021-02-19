20681ea3b4d407b20fce565e46936ecfe4a007e2
frida stenberg <fridastenberg97@hotmail.com>
2021-02-19 13:55:21 +0100

    added wordToGuess to WordOutput

--- src/components/WordOutput.js
+++ src/components/WordOutput.js
@@ -4,12 +4,13 @@ import  {useContext } from "react";
 import { HangmanContext } from "../context/HangmanContext";
 
 const WordOutput = () => {
-    
+const { wordToGuess} = useContext(HangmanContext);
+
     return (
         <div className="WordOutput">
             <div className="wordWrapper">
                 <div className="letterBox">
-                    
+                    <p>{wordToGuess}</p>
                 </div>
             </div>
         </div>
