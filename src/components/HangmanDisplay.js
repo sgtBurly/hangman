0ed059db5a9eadf345fb5ac722c8c6a95ae8db0f
Christina <chrasse@hotmail.com>
2021-02-19 12:53:56 +0100

    Added conditional rendering of classes.
    If game isn't started yet(lives === null), hangmanContainer will not be displayed. Depending on how many lives user has left, the different divs that the hangman consists of will be displayed.

--- src/components/HangmanDisplay.js
+++ src/components/HangmanDisplay.js
@@ -5,23 +5,24 @@ import { useContext, useState } from 'react'
 const HangmanDisplay = () => {
 
   const { lives } = useContext(HangmanContext);
-  // Uses 'dummy' variable instead, will use lives later
-  const [numberOfGuessesLeft, setNumberOfGuessesLeft] = useState(10);
+  // Uses 'dummy' variable instead, will use lives from Context later
+  const [numberOfGuessesLeft, setNumberOfGuessesLeft] = useState(3);
 
   return (
     <div className={styles.hangmanDisplay}>
+      {lives !== null &&
       <div className={styles.hangmanContainer}>
-        <div className={`${styles.foundation} ${styles.hidden}`}></div>
-        <div className={`${styles.verticalPole} ${styles.hidden}`}></div>
-        <div className={`${styles.horizontalPole} ${styles.hidden}`}></div>
-        <div className={`${styles.diagonal} ${styles.hidden}`}></div>
-        <div className={`${styles.rope} ${styles.hidden}`}></div>
-        <div className={`${styles.head} ${styles.hidden}`}></div>
-        <div className={`${styles.body} ${styles.hidden}`}></div>
-        <div className={`${styles.arms} ${styles.hidden}`}></div>
-        <div className={`${styles.legLeft} ${styles.hidden}`}></div>
-        <div className={`${styles.legRight} ${styles.hidden}`}></div>
-      </div>
+        <div className={`${styles.foundation} ${lives < 10 ? '' : styles.hidden}`}></div>
+        <div className={`${styles.verticalPole} ${lives < 9 ? '' : styles.hidden}`}></div>
+        <div className={`${styles.horizontalPole} ${lives < 8 ? '' : styles.hidden}`}></div>
+        <div className={`${styles.diagonal} ${lives < 7 ? '' : styles.hidden}`}></div>
+        <div className={`${styles.rope} ${lives < 6 ? '' : styles.hidden}`}></div>
+        <div className={`${styles.head} ${lives < 5 ? '' : styles.hidden}`}></div>
+        <div className={`${styles.body} ${lives < 4 ? '' : styles.hidden}`}></div>
+        <div className={`${styles.arms} ${lives < 3 ? '' : styles.hidden}`}></div>
+        <div className={`${styles.legLeft} ${lives < 2 ? '' : styles.hidden}`}></div>
+        <div className={`${styles.legRight} ${lives < 1 ? '' : styles.hidden}`}></div>
+      </div>}
     </div>
   );
 }
