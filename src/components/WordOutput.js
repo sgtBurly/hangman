6b21ee1271a4542eed8ce56e7a3bfd8873e3bbea
Christina <chrasse@hotmail.com>
2021-02-22 13:07:08 +0100

    Trying to make it work...

--- src/components/WordOutput.js
+++ src/components/WordOutput.js
@@ -1,35 +1,62 @@
 import react from "react";
 import style from "../styles/WordOutput.module.css"
-import  { useContext } from "react";
+import  { useContext, useEffect, useState } from "react";
 import { HangmanContext } from "../context/HangmanContext";
 
 const WordOutput = () => {
     const { splitGuessedWord } = useContext(HangmanContext);
+
     // Later... variable from Context
-    const guessedLetter = 'o';
+    //const { correctLetters } = useContext(HangmanContext);
+    const correctLetter = 'o';
     console.log(splitGuessedWord);
-    const emptyArray = [];
+    const [outputArray, setOutputArray] = useState([]);
+    console.log(outputArray);
 
-    if(splitGuessedWord){
-        splitGuessedWord.forEach(letter => {
-             emptyArray.push("");
-        });
-        console.log(emptyArray);
+    useEffect(()=>{
+        if(splitGuessedWord){
+            const tempArray = [];
+            splitGuessedWord.forEach(letter => {
+                 tempArray.push("");
+            });
+            setOutputArray(tempArray)
+            console.log(outputArray);
 
-        //User guesses a right letter. Will be variable from Context and updated with useEffect
-        splitGuessedWord.forEach((letter, i) => {
-            if (letter === guessedLetter){
-                emptyArray[i] = letter
+
+            //const index = splitGuessedWord.indexOf(guessedLetter);
+            //emptyArray[index] = guessedLetter;
+        }
+    }, [splitGuessedWord]);
+
+    useEffect(()=> {
+
+         //User guesses a right letter. Will be variable from Context and updated with useEffect
+         if(outputArray && splitGuessedWord) {
+            splitGuessedWord.forEach((letter, i) => {
+            if (letter === correctLetter){
+                setOutputArray([...outputArray.slice(0,i), letter, ...outputArray.slice(i+1,outputArray.length-1)])
+                //setOutputArray([...outputArray.splice(i, 0, letter)])
+                //outputArray[i] = letter
             }
-        })
-        //const index = splitGuessedWord.indexOf(guessedLetter);
-        //emptyArray[index] = guessedLetter;
-    }
+        })}
+    }, [correctLetter])
+
+
+    // useEffect(() => {
+    //     if(correctLetters){
+    //         splitGuessedWord.forEach((letter, i) => {
+    //             if (letter === correctLetters[correctLetters.length-1]){
+    //                 outputArray[i] = letter
+    //             }
+    //         })
+    //     }
+    // }, [correctLetters]);
+
 
     return (
         <div className={style.WordOutput}>
             <div className={style.wordWrapper}>
-                {splitGuessedWord && emptyArray.map((letter, i) => (
+                {splitGuessedWord && outputArray.map((letter, i) => (
                     <div className={style.LetterBox} key={i}>
                     <p className={style.Letter}>{letter}</p>
                 </div>
