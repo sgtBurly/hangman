dc5abe876d47b0814b72ff3dc2ff467ad4919311
Christina <chrasse@hotmail.com>
2021-02-22 10:56:16 +0100

    First update towards showing correct guesses

--- src/components/WordOutput.js
+++ src/components/WordOutput.js
@@ -4,12 +4,32 @@ import  { useContext } from "react";
 import { HangmanContext } from "../context/HangmanContext";
 
 const WordOutput = () => {
-const { splitGuessedWord } = useContext(HangmanContext);
+    const { splitGuessedWord } = useContext(HangmanContext);
+    // Later... variable from Context
+    const guessedLetter = 'o';
+    console.log(splitGuessedWord);
+    const emptyArray = [];
+
+    if(splitGuessedWord){
+        splitGuessedWord.forEach(letter => {
+             emptyArray.push("");
+        });
+        console.log(emptyArray);
+
+        //User guesses a right letter. Will be variable from Context and updated with useEffect
+        splitGuessedWord.forEach((letter, i) => {
+            if (letter === guessedLetter){
+                emptyArray[i] = letter
+            }
+        })
+        //const index = splitGuessedWord.indexOf(guessedLetter);
+        //emptyArray[index] = guessedLetter;
+    }
 
     return (
         <div className={style.WordOutput}>
             <div className={style.wordWrapper}>
-                {splitGuessedWord && splitGuessedWord.map((letter, i) => (
+                {splitGuessedWord && emptyArray.map((letter, i) => (
                     <div className={style.LetterBox} key={i}>
                     <p className={style.Letter}>{letter}</p>
                 </div>
